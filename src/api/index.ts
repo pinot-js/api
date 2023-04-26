ccded48ba5a3880ef5c633aa177f712acd5ff721
Jeeyong Um <conr2d@proton.me>
2023-04-26 13:50:36 +0900

    feat: Re-export internal @polkadot/api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,6 @@
 // Copyright (C) 2023 Haderech Pte. Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './promise/Api.js';
+export { ApiPromise } from './promise/Api.js';
+
+export * from '@polkadot/api';
